bd1338f03616a0ff859822a12c8c59912feece16
Troy Neubauer <troyneubauer@gmail.com>
2018-08-24 15:00:21 -0400

    Improved getInteger method

--- src/com/troy/labgrader/Utils.java
+++ src/com/troy/labgrader/Utils.java
@@ -1,8 +1,18 @@
 package com.troy.labgrader;
 
+import java.awt.Component;
+
+import javax.swing.JOptionPane;
+
 public class Utils {
+	
+	public static final int INVALID_STRING = Integer.MIN_VALUE;
+	
+	
+	
 	/**
 	 * Returns the integer representation of the number or {@link Integer#MIN_VALUE} if the string does not represent a valud int
+	 * 
 	 * @param string The string to parse
 	 * @return The string in an int form
 	 */
@@ -10,7 +20,11 @@ public class Utils {
 		try {
 			return Integer.parseInt(string);
 		} catch (NumberFormatException e) {
-			return Integer.MIN_VALUE;
+			return INVALID_STRING;
 		}
+	}
+
+	public static String getUserString(Component master, String message, String title, int messageType) {
+		return (String) JOptionPane.showInputDialog(master, message, title, messageType, null, null, null);
 	}
 }
