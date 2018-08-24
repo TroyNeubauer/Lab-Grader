ecf12667a08c69ce646b9371779709919997a168
Troy Neubauer <troyneubauer@gmail.com>
2018-08-24 15:01:04 -0400

    Began test code for table

--- src/com/troy/labgrader/Main.java
+++ src/com/troy/labgrader/Main.java
@@ -1,24 +1,35 @@
 package com.troy.labgrader;
 
 import java.awt.Font;
+import java.util.*;
 
-import javax.swing.UIManager;
+import javax.swing.*;
 
-import com.troy.labgrader.email.*;
-import com.troy.labgrader.ui.Window;
+import com.troy.labgrader.email.Student;
+import com.troy.labgrader.ui.*;
 
 public class Main {
+	
+	public static Window window;
 
 	public static void main(String[] args) throws Exception {
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		UIManager.getLookAndFeelDefaults().put("defaultFont", new Font("Arial", Font.BOLD, 30));
-		Window window = new Window();
+		List<Student> list = new ArrayList();
+		JFrame test = new JFrame();
+		test.add(new JTable(new FieldTabel<>(list, Student.class)));
+		
+		test.setSize(1440, 810);
+		test.setLocationRelativeTo(null);
+		test.setVisible(true);
+		
+		/*window = new Window();
 
 		EmailScanner scanner = new EmailScanner(new EmailListener() {
 			@Override
 			public void onEmail(Email email) {
 				FileUtils.saveEmail(email);
 			}
-		});
+		});*/
 	}
 }
