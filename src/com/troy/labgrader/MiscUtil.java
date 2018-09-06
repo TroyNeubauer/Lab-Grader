1713479cac0d39937bc8a260e6b48318afd377d0
Troy Neubauer <troyneubauer@gmail.com>
2018-09-06 14:58:00 -0400

    Fixed getExtension method in MiscUtil

--- src/com/troy/labgrader/MiscUtil.java
+++ src/com/troy/labgrader/MiscUtil.java
@@ -402,7 +402,7 @@ public class MiscUtil {
 	public static String getExtension(String name) {
 		int index = name.lastIndexOf('.');
 		if (index == -1)
-			return null;
+			return name;
 		return name.substring(index + 1);
 	}
 
